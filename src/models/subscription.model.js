6e71fc853e3500395e4c6bad3ada2eab4039b3e6
Abhishek Dubey <7abhishek1410@gmail.com>
2024-02-12 23:18:20 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const subscriptionSchema = new mongoose.Schema(
+    {
+        subscriber:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        channel:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }
+)
+
+export const Subscription  = mongoose.model('Subscription', subscriptionSchema)
+
+// subscriber and channels both are users only
